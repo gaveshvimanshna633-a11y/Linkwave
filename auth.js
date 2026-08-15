42651f9b771d5707535529b11fdec30df42a3b09
gaveshvimanshna633-a11y <gaveshvimanshna633@gmail.com>
2026-08-16 01:09:20 +0530

    Delete auth.js

--- auth.js
+++
@@ -1,3 +0,0 @@
-function signup(){auth.createUserWithEmailAndPassword(email.value,password.value).then(()=>alert("Created"));}
-function loginUser(){auth.signInWithEmailAndPassword(email.value,password.value).then(()=>alert("Login OK"));}
-function logout(){auth.signOut();}
